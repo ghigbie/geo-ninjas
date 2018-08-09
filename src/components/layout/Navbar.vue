f05a3391d9092b6deb50fd1e1e3513cb8259cc0b
ghigbie <gehigbie@gmail.com>
2018-08-08 20:17:30 -0600

    Add Markup

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
-    
+    <div class="navbar">
+        <div class="deep-purple">
+            <div class="container">
+                <a href="" class="band-logo">GeoNinjas!</a>
+                <ul class="right">
+                    <li><a href="">Signup</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 </template>
 
