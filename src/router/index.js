5cb96f2014bb86ccd140a8a9ae72eae336d5c71d
ghigbie <gehigbie@gmail.com>
2018-08-09 19:44:02 -0600

    Import Signup route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import GMap from '@/components/home/GMap';
+import Signup from '@/components/auth/Signup';
 
 Vue.use(Router)
 
@@ -11,5 +12,10 @@ export default new Router({
       name: 'GMap',
       component: GMap
     },
+    {
+      path: '/Signup',
+      name: 'Signup',
+      component: Signup
+    }
   ]
 })
