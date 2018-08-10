2e4816e80c671469fab1609011dfeb4751fca38d
ghigbie <gehigbie@gmail.com>
2018-08-09 19:51:45 -0600

    Add more fields

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -7,8 +7,15 @@
                 <input type="email" name="email" />
             </div>
             <div class="field">
-                <label for="email">Email:</label>
-                <input type="email" name="email" />
+                <label for="password">Password:</label>
+                <input type="password" name="password" />
+            </div>
+            <div class="alias">
+                <label for="alias">Alias:</label>
+                <input type="alias" name="alias" />
+            </div>
+            <div class="field center">
+                <button class="btn deep-purple">Submit</button>
             </div>
         </form>
     </div>
