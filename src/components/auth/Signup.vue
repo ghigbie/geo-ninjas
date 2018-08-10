9a72a3e6ce02437ba3b4c6bd0b35213fd4d8a463
ghigbie <gehigbie@gmail.com>
2018-08-09 19:41:46 -0600

    Add Signup component

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="signup">
+        <h1>Signup</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data(){
+        return {
+
+        };
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
