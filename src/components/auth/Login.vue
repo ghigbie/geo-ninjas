9ee283a80b14e13d602c4657c284ba8cbb113b91
ghigbie <gehigbie@gmail.com>
2018-08-11 17:21:28 -0600

    Add login form

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,6 +1,20 @@
 <template>
-    <div class="login">
-
+    <div class="login container">
+        <form class="card-panel">
+            <h2 class="center deep-purple-text">Login</h2>
+            <div class="field">
+                <label for="email">Email:</label>
+                <input type="email" 
+                       name="email" 
+                       v-model="email"/>
+            </div>
+            <div class="field">
+                <label for="password">Password:</label>
+                <input type="password" 
+                       name="password"
+                       v-model="password"/>
+            </div>
+        </form>
     </div>
 </template>
 
