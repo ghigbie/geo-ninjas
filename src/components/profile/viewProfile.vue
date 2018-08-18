5b282189a9672de7e5e6df05c8ae75f2d3f98f1e
ghigbie <gehigbie@gmail.com>
2018-08-17 20:03:45 -0600

    Add ViewProfile template

---
+++ src/components/profile/viewProfile.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="view-profile container">
+        <div class="card">
+            <h2 class="deep-purple-text center">User's Wall</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            profile: null
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
